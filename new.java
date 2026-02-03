6e757ff4c240b3e76b4334083eee017b5cc75d0f
Joseph DeCommer <decommer@icloud.com>
2026-02-03 13:27:29 -0500

    tesr

--- new.java
+++ new.java
@@ -1,10 +1,9 @@
-public class New {
-
+class New {
 
     public static void main(String[] args) {
 
-        for(int i = 0; i < 101; i++) {
-            System.out.print(i - 1 + ", ");
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i);
         }
     }
 }
